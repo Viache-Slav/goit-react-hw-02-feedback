032c6f5f7b0d5361182cbd97bddfa7f2ad182f1c
Viache-Slav <slavikfedko@gmail.com>
2023-10-24 18:42:11 +0200

    finish
    utworzony moduł Notification, dodano stylizację komponentów, propTypes

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import css from 'components/FeedbackOptions/FeedbackOptions.module.css'
+import PropTypes from 'prop-types';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <div>
     {options.map(option => (
-      <button key={option} onClick={() => onLeaveFeedback(option)}>
+      <button 
+      className={css.button}
+      key={option} 
+      onClick={() => onLeaveFeedback(option)}
+      type="button"
+      >
         {option}
       </button>
     ))}
   </div>
 );
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
 export default FeedbackOptions;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import css from 'components/Section/Section.module.css'
+import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => (
-  <section>
+  <section className={css.section}>
     <h2>{title}</h2>
     {children}
   </section>
 );
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Section;
